6b5bfed0e24b614028ac4eb0b40db1460023d641
e1ing <elvira.kisling@gmail.com>
2021-05-10 21:44:35 +0300

    homework-3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -2,27 +2,28 @@ import React from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: any) => void  // need to fix any
+    addUser: () => void// need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    let inputClass = error ? s.error : s.inputClass // need to fix with (?:)
 
     return (
         <div>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
             <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <button onClick={addUser}
+            disabled={!name}>add</button>
             <span>{totalUsers}</span>
         </div>
-    )
+    );
 }
 
 export default Greeting
