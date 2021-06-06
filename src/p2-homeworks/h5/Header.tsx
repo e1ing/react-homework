9b9a76047cb1ecad3969191e49d549d655d9b2c4
e1ing <elvira.kisling@gmail.com>
2021-06-06 17:50:43 +0300

    homework-5 without menu button

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import s from './Header.module.css';
+
 
 function Header() {
+    // @ts-ignore
     return (
-        <div>
-            // add NavLinks
+        <>
+            <div className={s.header}>
+                <NavLink className={s.nav} activeClassName={s.activeNav} to="/pre-junior"> Pre-junior</NavLink>
+                <NavLink className={s.nav} activeClassName={s.activeNav} to="/junior">Junior</NavLink>
+                <NavLink className={s.nav} activeClassName={s.activeNav} to="/junior-plus"> Junior-plus</NavLink>
+                <div className = {s.menuBtn}/>
+            </div>
 
-        </div>
+        </>
     )
 }
 
