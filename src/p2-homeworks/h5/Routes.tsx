2b21f78e1b1c0ff68903dd2624d9873ec377bcc7
e1ing <elvira.kisling@gmail.com>
2021-06-06 21:42:01 +0300

    homework-7

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
+import Junior from "./pages/Junior";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -21,6 +22,7 @@ function Routes() {
             <Route path={'/'} exact render={() => <Redirect to={PATH.JUNIOR_PLUS}/>}/>
                 {/*add routes*/}
             <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
+            <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             <Route render={() => <Error404/>}/>
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HW7 from '../../h7/HW7'
+
+function Junior() {
+    return (
+        <div>
+            <HW7/>
+        </div>
+    )
+}
+
+export default Junior;
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
